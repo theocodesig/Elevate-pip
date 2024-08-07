13d9d14a1af5589170186564e67c4305d5ed32b0
bigkenzello <90573625+bigkenzello@users.noreply.github.com>
2024-08-07 17:32:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ from similars import similar_artists
 import threading
 
 def main():
-    # thread1 = threading.Thread(target=multiSearch)
-    # thread2 = threading.Thread(target=similar_artists)
+    thread1 = threading.Thread(target=multiSearch)
+    thread2 = threading.Thread(target=similar_artists)
 
-    # thread1.start()
-    # thread2.start()
+    thread1.start()
+    thread2.start()
 
-    # thread1.join()
-    # thread2.join()
+    thread1.join()
+    thread2.join()
 
     multiSearch()
 
